8d7f92b3eb1af335e50bdcaa02be37f98a78cf4c
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-01-16 09:56:42 -0300

    Add atribute validation

--- src/main/java/com/springcourse/springcourse/dto/UserLogindto.java
+++ src/main/java/com/springcourse/springcourse/dto/UserLogindto.java
@@ -5,10 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
 public class UserLogindto {
+
+    @Email(message = "Invalid email address")
     private String email;
+
+    @NotBlank(message = "Password required!")
     private String password;
 }
--- src/main/java/com/springcourse/springcourse/dto/UserUpdateRoledto.java
+++ src/main/java/com/springcourse/springcourse/dto/UserUpdateRoledto.java
@@ -4,8 +4,12 @@ import com.springcourse.springcourse.domain.enums.Role;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
+
 @Getter @Setter
 public class UserUpdateRoledto {
 
+    @NotNull(message = "Role Required")
     private Role role;
 }
