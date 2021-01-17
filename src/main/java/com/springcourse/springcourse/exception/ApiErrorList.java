9efc277a20fc043f17ae4d04512871f84c0eafd3
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-01-17 09:09:50 -0300

    Create api error list class

---
+++ src/main/java/com/springcourse/springcourse/exception/ApiErrorList.java
@@ -0,0 +1,31 @@
+package com.springcourse.springcourse.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter @Setter
+public class ApiErrorList extends ApiError{
+
+    private static final  long serialVersionUID = 1l;
+
+    private List<String> errors;
+
+    public ApiErrorList(int code, String msg, Date date, List<String> errors){
+        super(code,msg,date);
+        this.errors = errors;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
