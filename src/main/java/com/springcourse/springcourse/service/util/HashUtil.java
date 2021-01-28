a9fefe57761eedfe37858d3596984c8c5c8ce145
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-01-28 20:42:45 -0300

    Created security config

--- src/main/java/com/springcourse/springcourse/service/util/HashUtil.java
+++ src/main/java/com/springcourse/springcourse/service/util/HashUtil.java
@@ -5,7 +5,6 @@ import org.apache.commons.codec.digest.DigestUtils;
 public class HashUtil {
 
     public static String getSecurityHash(String text){
-
         String hash = DigestUtils.sha256Hex(text);
         return hash;
     }
