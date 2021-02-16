63cdcb23e79600e0ea967daa61e178d696d39491
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-02-16 15:41:37 -0300

    Create Request file repository

---
+++ src/main/java/com/springcourse/springcourse/repository/RequestFileRepository.java
@@ -0,0 +1,13 @@
+package com.springcourse.springcourse.repository;
+
+import com.springcourse.springcourse.domain.RequestFile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RequestFileRepository extends JpaRepository<RequestFile, Long> {
+
+    public Page<RequestFile> findAllByRequestId(Long id, Pageable pageable);
+}
