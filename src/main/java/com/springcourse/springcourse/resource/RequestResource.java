624a969f52524b7f8318720945c67b25a0c39244
flavionfg <44682348+flavionfg@users.noreply.github.com>
2021-02-17 20:14:42 -0300

    Integrate request resource with request file service
    Add method to list request files,
    Add method to upload request files.

--- src/main/java/com/springcourse/springcourse/resource/RequestResource.java
+++ src/main/java/com/springcourse/springcourse/resource/RequestResource.java
@@ -1,12 +1,14 @@
 package com.springcourse.springcourse.resource;
 
 import com.springcourse.springcourse.domain.Request;
+import com.springcourse.springcourse.domain.RequestFile;
 import com.springcourse.springcourse.domain.RequestStage;
 import com.springcourse.springcourse.dto.RequestSavedto;
 import com.springcourse.springcourse.dto.RequestUpdatedto;
 import com.springcourse.springcourse.model.PageModel;
 import com.springcourse.springcourse.model.PageRequestModel;
 import com.springcourse.springcourse.security.AccessManager;
+import com.springcourse.springcourse.service.RequestFileService;
 import com.springcourse.springcourse.service.RequestService;
 import com.springcourse.springcourse.service.RequestStageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +16,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "requests")
@@ -29,6 +33,9 @@ public class RequestResource {
 
     @Autowired
     private AccessManager accessManager;
+
+    @Autowired
+    private RequestFileService fileService;
 
     @PostMapping
     public ResponseEntity<Request> save (@RequestBody @Valid RequestSavedto requestdto){
@@ -67,10 +74,25 @@ public class RequestResource {
             @PathVariable(name = "id") Long id,
             @RequestParam(value = "page",defaultValue = "0") int page,
             @RequestParam(value = "size",defaultValue = "10") int size){
+            PageRequestModel pr = new PageRequestModel(page,size);
+            PageModel<RequestStage> pm = requestStageService.listAllByRequestIdOnLazyMode(id, pr);
+            return ResponseEntity.ok(pm);
+    }
 
-    PageRequestModel pr = new PageRequestModel(page,size);
-    PageModel<RequestStage> pm = requestStageService.listAllByRequestIdOnLazyMode(id, pr);
+    @GetMapping("/{id}/files")
+    public ResponseEntity<PageModel<RequestFile>> listAllFilesById(
+            @PathVariable(name = "id") Long id,
+            @RequestParam(value = "page",defaultValue = "0") int page,
+            @RequestParam(value = "size",defaultValue = "10") int size){
+        PageRequestModel pr = new PageRequestModel(page,size);
+        PageModel<RequestFile> pm = fileService.listAllByRequestId(id,pr);
+        return ResponseEntity.ok(pm);
+    }
 
-    return ResponseEntity.ok(pm);
+    @PostMapping("/{id}/files")
+    public ResponseEntity<List<RequestFile>> upload(@RequestParam("files")MultipartFile[] files, @PathVariable(name = "id") Long id){
+        List<RequestFile> requestFiles = fileService.upload(id,files);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(requestFiles);
     }
 }
